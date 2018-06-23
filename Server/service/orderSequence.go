c902387def1c5c7db82a6ecfcda096628c26570b
mfslog <Jerry.Chau@qq.com>
2018-06-23 12:54:13 +0800

    增加按月，按年重置
    1.增加按月重置SEQ
    2.增加按年充值SEQ

--- Server/service/orderSequence.go
+++ Server/service/orderSequence.go
@@ -11,25 +11,37 @@ func GetOrderSequnce(firstId int32, secondId int32)string{
     seqRecord := DAO.TSequenceNumber{}
     seqRecord.GetOneByBusinessID(firstId, secondId)
     currentTime := time.Now()
-    //判断序号reset类型
-    var lastRestTimeStamp int64 = int64(seqRecord.LastResetTime)
-    lastResetTime := time.Unix( lastRestTimeStamp,0)
-    switch(seqRecord.RsetType){
+
+    var curResetStamp int32
+    //1.获得当前重置时间
+    switch(seqRecord.RsetType){//判断序号reset类型
     case 1: //按天重置
-        if currentTime.After(lastResetTime.Add(time.Hour*24)) {
-            currentTimeStr := currentTime.Format("2006-01-02")
-            curDayTime,_ := time.ParseInLocation("2006-01-02",currentTimeStr,time.Local)
-            curDayStamp := curDayTime.Unix()
-            var tmp int32 = int32(curDayStamp)
-            log.Debug(0,fmt.Sprintf("value:%d",tmp))
-            //重置sequence
-            seqRecord.ResetSeqByBussinessID(firstId,secondId,tmp)
-            //重新获得sequence值
-            seqRecord.GetOneByBusinessID(firstId,secondId)
-        }
+        //1.获得当前日期的零点
+        currentTimeStr := currentTime.Format("2006-01-02")
+        curDayTime,_ := time.ParseInLocation("2006-01-02",currentTimeStr,time.Local)
+        curResetStamp = int32(curDayTime.Unix())
+    case 2: //按月重置
+        //1.获得xx-01 零点时间
+        currentMonthStr := currentTime.Format("2006-01")
+        currentMonthTime,_ := time.ParseInLocation("2006-01",currentMonthStr,time.Local)
+        curResetStamp = int32(currentMonthTime.Unix())
+
+    case 3: //按年重置
+        currentYearStr := currentTime.Format("2006")
+        currentYearTime,_ := time.ParseInLocation("2006",currentYearStr,time.Local)
+        curResetStamp = int32(currentYearTime.Unix())
     }
-    
+
+    //2.判断重置
+    if curResetStamp> seqRecord.LastResetTime {
+        //重置sequence
+        seqRecord.ResetSeqByBussinessID(firstId,secondId,curResetStamp)
+        //重新获得sequence值
+        seqRecord.GetOneByBusinessID(firstId,secondId)
+    }
+
     newValue := seqRecord.CurrentValue + int64(seqRecord.StepLength)
+
     var sequence string
     if newValue < seqRecord.MaxValue {
         curstr := currentTime.Format("20060102")
@@ -39,6 +51,5 @@ func GetOrderSequnce(firstId int32, secondId int32)string{
         log.Error(0,fmt.Sprintf("error current sequence for [%d:%d] Depletion",firstId,secondId))
         sequence = ""
     }
-    
     return sequence
 }
